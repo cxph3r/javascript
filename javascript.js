7d6a4375afdee20bd0cd7cc9e96192fb25e29b00
cxph3r <bsrikarthikeya@gmail.com>
2024-10-14 20:38:16 +0530

    Add javascript.js to index.html

---
+++ javascript.js
@@ -0,0 +1,15 @@
+console.log(33 + 47);
+console.log(12 + 13 + 14 + 15 + 16 + 17 + 18);
+console.log((4 + 6 + 9) / 77);
+
+
+let a = 10;
+console.log(a);
+console.log(9 * a);
+let b = 7 * a;
+console.log(b);
+
+const max = 57;
+let actual = max - 13;
+let percentage = actual/max;
+console.log(percentage);
